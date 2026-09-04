79627fae6c22cd5f0264cec328dd1f9b82125e6f
MessoremVL <L26550256@chihuahua2.TECNM.MX>
2026-09-03 18:44:13 -0600

    Add files via upload

---
+++ EVA1_3_SALIDAS/src/eva1_3_salidas/EVA1_3_SALIDAS.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_3_salidas;
+
+/**
+ *
+ * @author bisonte
+ */
+public class EVA1_3_SALIDAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Bienvenido a Fundamenos De Programación");
+        System.out.println("Marco Antonio Ibarvo Vazquez");
+        System.out.print("Tu Calificación será:");
+        System.out.println(85);
+        
+    }
+    
+}
